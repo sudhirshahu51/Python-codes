78ffd26d6ed97b42218dc588d76a2cf1a674b53b
Sudhir Kumar Shahu <sudhirkumarshahu80@gmail.com>
2017-05-18 20:26:55 +0530

    Update send_text.py

--- send_text.py
+++ send_text.py
@@ -1,11 +1,11 @@
-from twilio.rest import TwilioRestClient
+from twilio import rest
 
 # Your Account SID from twilio.com/console
 account_sid = "AC7c87e29cd0de18d62eab8c239154cb04"
 # Your Auth Token from twilio.com/console
 auth_token  = "06c10ee61fdc1368611e119babd6f94c"
 
-client = TwilioRestClient(account_sid, auth_token)
+client = rest.TwilioRestClient(account_sid, auth_token)
 
 message = client.messages.create(
     to="+919425656165", 
