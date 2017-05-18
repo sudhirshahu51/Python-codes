1cf1dd3f54a5fcdd73ef7d56ad1a8565b585407e
Sudhir Kumar Shahu <sudhirkumarshahu80@gmail.com>
2017-05-18 20:25:36 +0530

    Create send_text.py

---
+++ send_text.py
@@ -0,0 +1,15 @@
+from twilio.rest import TwilioRestClient
+
+# Your Account SID from twilio.com/console
+account_sid = "AC7c87e29cd0de18d62eab8c239154cb04"
+# Your Auth Token from twilio.com/console
+auth_token  = "06c10ee61fdc1368611e119babd6f94c"
+
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+919425656165", 
+    from_="+16193206991",
+    body="kya ho rha??")
+
+print(message.sid)
