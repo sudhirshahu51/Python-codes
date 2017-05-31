4a5125164ed73559b910314d1ab795d3045d5b48
Sudhir Kumar Shahu <sudhirkumarshahu80@gmail.com>
2017-05-31 23:04:16 +0530

    Add files via upload

---
+++ entertainment _center.py
@@ -0,0 +1,12 @@
+import media
+toy_story = media.Movie("Toy Story",
+                        " A story of a boy and his toys that come to life",
+                        "https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+#print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+                    "A marine on an alien planet",
+                    "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+                    "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+print(avatar.storyline)
+
